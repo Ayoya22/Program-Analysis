3f81046ffbd05acc9a0c50de311de1461678a39e
Ayoya22 <djoukamsimplice@gmail.com>
2019-12-08 04:18:33 +0100

    Created a simple program to count the characters in a directory.

---
+++ sample-1.py
@@ -0,0 +1,13 @@
+import sys, os
+
+#count the total number of characters for each file in directory
+def countchars(directory):
+    n = 0
+    for name in os.listdir(directory):
+        fp = open(name)
+        for line in fp:
+            n += line
+        fp.fclose()
+    return n
+
+countchars(sys.argv[1])    
