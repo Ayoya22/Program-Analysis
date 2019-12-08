b1649791dcae8aed1ee38847d941c51c7a23fce3
Ayoya22 <djoukamsimplice@gmail.com>
2019-12-08 04:11:47 +0100

    Created a sample python File.

--- Analyse.py
+++ Analyse.py
@@ -2,7 +2,7 @@ print("This program is going to analyse every instruction of your program"
       " and return ranges of values each variable of your program may contain")
 
 ins_num = int(input("How many instructions does your program has: "))
-init = 0
-while init < ins_num:
-      print(f'Type the number {init} init: ')
+init = 1
+while init <= ins_num:
+      print(f'Type the number {init} instruction: ')
       init = init + 1
